fd91f8904bc02b3cd9799085c7beebae7391a75f
iqra-akram-sqa <iqraakram834@gmail.com>
2026-01-21 05:01:28 +0500

    Initial Cypress framework setup

---
+++ cypress/integration/example.spec.js
@@ -0,0 +1,7 @@
+describe('My First Cypress Test', () => {
+    it('Visits example.com and checks the title', () => {
+      cy.visit('https://example.com');
+      cy.title().should('include', 'Example Domain');
+    });
+  });
+  
